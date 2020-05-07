465a0b3430a0e670043972e9fb441ca74bd3eba3
Paul Ambrose <pambrose@mac.com>
2020-05-07 09:46:52 -0700

    Update content
    * Update content

--- src/Content.kt
+++ src/Content.kt
@@ -2,13 +2,8 @@ import com.github.readingbat.ReadingBatServer
 import com.github.readingbat.dsl.GitHubContent
 import com.github.readingbat.dsl.include
 import com.github.readingbat.dsl.readingBatContent
+import mu.KLogging
 
-object Main {
-  @JvmStatic
-  fun main(args: Array<String>) {
-    ReadingBatServer.start(siteContent)
-  }
-}
 
 val organization = "readingbat"
 val javaRepo = "readingbat-java-content"
@@ -21,3 +16,10 @@ val siteContent =
     +include(GitHubContent(organization, pythonRepo, branch = branch, srcPath = "src")).python
     +include(GitHubContent(organization, javaRepo, branch = branch)).kotlin
   }
+
+object Main : KLogging() {
+  @JvmStatic
+  fun main(args: Array<String>) {
+    ReadingBatServer.start(siteContent)
+  }
+}
