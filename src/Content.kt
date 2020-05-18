b8e70188414f4ef4c0809a34a3e68d6a4db1c2d9
Paul Ambrose <pambrose@mac.com>
2020-05-18 14:34:09 -0700

    Dev
    * Update content

--- src/Content.kt
+++ src/Content.kt
@@ -1,25 +1,15 @@
-import com.github.readingbat.ReadingBatServer
 import com.github.readingbat.dsl.GitHubContent
-import com.github.readingbat.dsl.include
+import com.github.readingbat.dsl.eval
 import com.github.readingbat.dsl.readingBatContent
-import mu.KLogging
-
 
 val organization = "readingbat"
 val javaRepo = "readingbat-java-content"
 val pythonRepo = "readingbat-python-content"
 val branch = "master"
 
-val siteContent =
+val content =
   readingBatContent {
-    +include(GitHubContent(organization, javaRepo, branch = branch)).java
-    +include(GitHubContent(organization, pythonRepo, branch = branch, srcPath = "src")).python
-    +include(GitHubContent(organization, javaRepo, branch = branch)).kotlin
+    include(GitHubContent(organization, javaRepo, branch = branch).eval().java)
+    include(GitHubContent(organization, pythonRepo, branch = branch, srcPath = "src").eval().python)
+    include(GitHubContent(organization, javaRepo, branch = branch).eval().kotlin)
   }
-
-object Main : KLogging() {
-  @JvmStatic
-  fun main(args: Array<String>) {
-    ReadingBatServer.start(siteContent)
-  }
-}
---
+++ src/ContentServer.kt
@@ -0,0 +1,8 @@
+import com.github.readingbat.ReadingBatServer
+
+object ContentServer {
+  @JvmStatic
+  fun main(args: Array<String>) {
+    ReadingBatServer.start(args)
+  }
+}
