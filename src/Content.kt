75b305c4ec700467bdf58cce3a0cfccdf0b3ac92
pambrose <pambrose@mac.com>
2020-07-30 21:26:22 -0700

    Add Athenian content

--- src/Content.kt
+++ src/Content.kt
@@ -1,15 +1,18 @@
+import com.github.pambrose.common.util.OwnerType.Organization
+import com.github.pambrose.common.util.OwnerType.User
 import com.github.readingbat.dsl.GitHubContent
 import com.github.readingbat.dsl.eval
 import com.github.readingbat.dsl.readingBatContent
 
-val organization = "readingbat"
+val org = "readingbat"
 val javaRepo = "readingbat-java-content"
 val pythonRepo = "readingbat-python-content"
-val branch = "master"
 
 val content =
   readingBatContent {
-    include(GitHubContent(organization, javaRepo, branch = branch).eval(this).java)
-    include(GitHubContent(organization, pythonRepo, branch = branch, srcPath = "src").eval(this).python)
-    include(GitHubContent(organization, javaRepo, branch = branch).eval(this).kotlin)
+    include(GitHubContent(Organization, org, javaRepo).eval(this).java)
+    include(GitHubContent(Organization, org, pythonRepo, srcPath = "src").eval(this).python)
+    include(GitHubContent(Organization, org, javaRepo).eval(this).kotlin)
+
+    include(GitHubContent(User, "maleich", "ReadingBat-content").eval(this).python, "Athenian")
   }
