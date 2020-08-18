2c4b455ab4b07ee26bf0a8bf0b43e234ba4c71ca
pambrose <pambrose@mac.com>
2020-08-17 19:41:31 -0700

    Add support for multiple browser session support

--- src/Content.kt
+++ src/Content.kt
@@ -4,15 +4,14 @@ import com.github.readingbat.dsl.GitHubContent
 import com.github.readingbat.dsl.eval
 import com.github.readingbat.dsl.readingBatContent
 
-val org = "readingbat"
 val javaRepo = "readingbat-java-content"
 val pythonRepo = "readingbat-python-content"
 
 val content =
   readingBatContent {
-    include(GitHubContent(Organization, org, javaRepo).eval(this).java)
-    include(GitHubContent(Organization, org, pythonRepo, srcPath = "src").eval(this).python)
-    include(GitHubContent(Organization, org, javaRepo).eval(this).kotlin)
+    include(GitHubContent(Organization, "readingbat", javaRepo).eval(this).java)
+    include(GitHubContent(Organization, "readingbat", pythonRepo, srcPath = "src").eval(this).python)
+    include(GitHubContent(Organization, "readingbat", javaRepo).eval(this).kotlin)
 
     include(GitHubContent(User, "maleich", "ReadingBat-content").eval(this).python, "Athenian: ")
   }
