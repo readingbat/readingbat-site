4a3a8207dd0b93f4c051911c0c5b410a82d4cb71
pambrose <pambrose@mac.com>
2020-08-22 19:42:14 -0700

    Update jars

--- src/Content.kt
+++ src/Content.kt
@@ -4,14 +4,11 @@ import com.github.readingbat.dsl.GitHubContent
 import com.github.readingbat.dsl.eval
 import com.github.readingbat.dsl.readingBatContent
 
-val javaRepo = "readingbat-java-content"
-val pythonRepo = "readingbat-python-content"
-
 val content =
   readingBatContent {
-    include(GitHubContent(Organization, "readingbat", javaRepo).eval(this).java)
-    include(GitHubContent(Organization, "readingbat", pythonRepo, srcPath = "src").eval(this).python)
-    include(GitHubContent(Organization, "readingbat", javaRepo).eval(this).kotlin)
+    include(GitHubContent(Organization, "readingbat", "readingbat-java-content").eval(this).java)
+    include(GitHubContent(Organization, "readingbat", "readingbat-python-content", srcPath = "src").eval(this).python)
+    include(GitHubContent(Organization, "readingbat", "readingbat-java-content").eval(this).kotlin)
 
     include(GitHubContent(User, "maleich", "ReadingBat-content").eval(this).python, "Athenian: ")
   }
